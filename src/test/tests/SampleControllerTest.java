a0d1fdc5bd571f93495e40ac8427807998d8861a
nidhibudhraja <nidhi.budhraja96@gmail.com>
2020-10-29 22:48:26 +0530

    added testing for multiple testcases

--- src/test/tests/SampleControllerTest.java
+++ src/test/tests/SampleControllerTest.java
@@ -1,6 +1,10 @@
 package  tests;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
 import javafx.scene.control.Button;
@@ -18,6 +22,8 @@ import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 //import org.junit.jupiter.params.ParameterizedTest;
 //import org.junit.jupiter.params.provider.CsvFileSource;
+
+import com.opencsv.CSVReader;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -44,11 +50,11 @@ class SampleControllerTest  {
 	}
 
 
-	//@Test
-	@ParameterizedTest(name = "{index} => nextLine[0]={0}, nextLine[1]={1}, nextLine[2]={2},nextLine[3]={3}, nextLine[4]={4}")
-	@CsvFileSource(resources = "/testcases.csv")
-	void loginTest(String nextLine[]) {
-		    FxRobot robot=new FxRobot();
+	@Test
+	//@ParameterizedTest(name = "{index} => nextLine[0]={0}, nextLine[1]={1}, nextLine[2]={2},nextLine[3]={3}, nextLine[4]={4}")
+	//@CsvFileSource(resources = "/testcases.csv")
+	void loginTest(FxRobot robot) {
+		   
 		
 			Button btnlogin=robot.lookup("#loginButton").queryAs(Button.class);
 			assertNotNull(btnlogin);
@@ -60,57 +66,69 @@ class SampleControllerTest  {
 			robot.write("1");
 			robot.clickOn(btnlogin);
 			assertEquals("Login", btnlogin.getText());	
-			fillform(robot,nextLine);
-		
+			//fillform(robot,nextLine);
+			fillform(robot);
 		
 		
 	}
 	
 	
-	void fillform(FxRobot robot,String nextLine[]) {
-	
-				robot.clickOn("#fnameTextField");
-				robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
-				//robot.write("alisha");
-				robot.write(nextLine[0]);
-				robot.clickOn("#lnameTextField");
-				robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
-				//robot.write("shourya");
-				robot.write(nextLine[1]);
-				Spinner<Integer>ageSpinner=robot.lookup("#ageSpinner").queryAs(Spinner.class);
-				ageSpinner.getValueFactory().increment(Integer.parseInt(nextLine[2])-1);
-				//ageSpinner.getValueFactory().increment(2);
-				if(checkParentPane(robot)) {
-					robot.clickOn("#pfnameTextField");
+//	void fillform(FxRobot robot,String nextLine[]) {
+	void fillform(FxRobot robot) {
+				CSVReader csvReader;
+		        String[] nextLine; 
+		  
+		        // we are going to read data line by line 
+		        try {
+		        	csvReader = new CSVReader(new FileReader("/testcases.csv"));
+					while ((nextLine = csvReader.readNext()) != null) { 
+
+					robot.clickOn("#fnameTextField");
 					robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
-					//robot.write("alka");
-					
-					robot.write(nextLine[3]);
-					robot.clickOn("#plnameTextField");
+					//robot.write("alisha");
+					robot.write(nextLine[0]);
+					robot.clickOn("#lnameTextField");
 					robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
-					//robot.write("sharma");
-					robot.write(nextLine[4]);
-					
-				}
-				Button btnNext=robot.lookup("#nextButton").queryAs(Button.class);
-				assertNotNull(btnNext);
-				robot.clickOn(btnNext);
-				TextField idimgTextField=robot.lookup("#imgTextField").queryAs(TextField.class);
+					//robot.write("shourya");
+					robot.write(nextLine[1]);
+					Spinner<Integer>ageSpinner=robot.lookup("#ageSpinner").queryAs(Spinner.class);
+					ageSpinner.getValueFactory().increment(Integer.parseInt(nextLine[2])-1);
+					//ageSpinner.getValueFactory().increment(2);
+					if(checkParentPane(robot)) {
+						robot.clickOn("#pfnameTextField");
+						robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
+						//robot.write("alka");						
+						robot.write(nextLine[3]);
+						robot.clickOn("#plnameTextField");
+						robot.press(KeyCode.CONTROL).press(KeyCode.A).release(KeyCode.A).release(KeyCode.CONTROL);
+						//robot.write("sharma");
+						robot.write(nextLine[4]);
+						
+					}
+					Button btnNext=robot.lookup("#nextButton").queryAs(Button.class);
+					assertNotNull(btnNext);
+					robot.clickOn(btnNext);
+					TextField idimgTextField=robot.lookup("#imgTextField").queryAs(TextField.class);
 //				Platform.runLater(
 //						  () -> {
 //						    // Update UI here.
 //							  idimgLabel.setText(nextLine[5]);
 //						  }
 //						);
-				idimgTextField.setText(nextLine[5]);
-				//idimgTextField.setText("/home/nidhi/Pictures/a.jpeg");
-				Button btnSubmit=robot.lookup("#submitButton").queryAs(Button.class);
-				assertNotNull(btnSubmit);
-				robot.clickOn(btnSubmit);
-//				Button btnPrev=robot.lookup("#prevButton").queryAs(Button.class);
-//				assertNotNull(btnPrev);
-//				robot.clickOn(btnPrev);
-				//putImage(robot);
+					idimgTextField.setText(nextLine[5]);
+					//idimgTextField.setText("/home/nidhi/Pictures/a.jpeg");
+					Button btnSubmit=robot.lookup("#submitButton").queryAs(Button.class);
+					assertNotNull(btnSubmit);
+					robot.clickOn(btnSubmit);
+					Button btnPrev=robot.lookup("#prevButton").queryAs(Button.class);
+					assertNotNull(btnPrev);
+					robot.clickOn(btnPrev);
+					//putImage(robot);
+					}
+				} catch (NumberFormatException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		
 	}
 	boolean checkParentPane(FxRobot robot) {
